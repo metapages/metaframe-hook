a19507d84d88d0d8e75d7b120ed1dba0be6be8d9
Dion Amago <dion@transition9.com>
2021-09-12 22:31:27 -0700

    hook for just the metaframe, do not want input listeners to be bound to react render cycles

--- src/hookMetaframe.tsx
+++ src/hookMetaframe.tsx
@@ -3,28 +3,48 @@ import React, { useContext, useEffect, useState, createContext } from "react";
 import { Metaframe, MetaframeInputMap } from "@metapages/metapage";
 
 export interface MetaframeObject {
-  inputs: MetaframeInputMap;
-  setOutputs?: (outputs: MetaframeInputMap) => void;
   // This is only set when initialized
   metaframe?: Metaframe;
 }
 
-const defaultMetaframeObject: MetaframeObject = {
+export interface MetaframeAndInputsObject extends MetaframeObject {
+  inputs: MetaframeInputMap;
+  setOutputs?: (outputs: MetaframeInputMap) => void;
+}
+
+const defaultMetaframeAndInputsObject: MetaframeAndInputsObject = {
   inputs: {},
 };
+
+const defaultMetaframeObject: MetaframeObject = {};
 
 export const MetaframeContext = createContext<MetaframeObject>(
   defaultMetaframeObject
 );
+export const MetaframeAndInputsContext =
+  createContext<MetaframeAndInputsObject>(defaultMetaframeAndInputsObject);
 
 export const useMetaframe = () => {
   return useContext(MetaframeContext);
 };
 
-export const WithMetaframe: React.FC<any> = (props: any) => {
-  const [metaframeObject, setMetaframeObject] = useState<MetaframeObject>({
-    inputs: {},
-  });
+export const useMetaframeAndInput = () => {
+  return useContext(MetaframeAndInputsContext);
+};
+
+/**
+ * For convenience, return inputs as they arrive as hooks.
+ * If performance is a concern, use WithMetaframe and listen
+ * to the inputs yourself (as implemented here, but not updating
+ * a hook function every single input update)
+ *
+ * @returns
+ */
+export const WithMetaframeAndInputs: React.FC<any> = (props: any) => {
+  const [metaframeObject, setMetaframeObject] =
+    useState<MetaframeAndInputsObject>({
+      inputs: {},
+    });
   const [metaframe, setMetaframe] = useState<Metaframe | undefined>(undefined);
   const [inputs, setInputs] = useState<MetaframeInputMap>(
     metaframeObject.inputs
@@ -53,6 +73,33 @@ export const WithMetaframe: React.FC<any> = (props: any) => {
       });
     }
   }, [inputs, metaframe]);
+
+  return (
+    <MetaframeAndInputsContext.Provider value={metaframeObject}>
+      {props.children}
+    </MetaframeAndInputsContext.Provider>
+  );
+};
+
+export const WithMetaframeOnly: React.FC<any> = (props: any) => {
+  const [metaframeObject, setMetaframeObject] = useState<MetaframeObject>({});
+  const [metaframe, setMetaframe] = useState<Metaframe | undefined>(undefined);
+
+  useEffect(() => {
+    const newMetaframe = new Metaframe();
+    setMetaframe(newMetaframe);
+    return () => {
+      newMetaframe.dispose();
+    };
+  }, [setMetaframe]);
+
+  useEffect(() => {
+    if (metaframe) {
+      setMetaframeObject({
+        metaframe,
+      });
+    }
+  }, [metaframe]);
 
   return (
     <MetaframeContext.Provider value={metaframeObject}>
