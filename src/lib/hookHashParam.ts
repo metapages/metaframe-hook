ae29c537f661c40396aad2c50a325453f77e48fd
robot <ci@rob.ot>
2021-10-04 00:16:26 +0000

    More better exports

--- src/lib/hookHashParam.ts
+++ src/lib/hookHashParam.ts
@@ -1,8 +1,6 @@
 import { useCallback, useEffect, useState } from "react";
+import { getHashParams, setHashParam, SetHashParamOpts } from "./util";
 
-export type SetHashParamOpts = {
-  modifyHistory?: boolean;
-};
 /**
  * Hook for getting/setting hash params
  */
@@ -28,85 +26,10 @@ export const useHashParam = (
 
   const setParam: (v: string | undefined) => void = useCallback(
     (value: string | undefined, opts?: SetHashParamOpts) => {
-      const paramHash = getHashParams()[1];
-
-      let changed = false;
-      if (
-        (paramHash.hasOwnProperty(key) && value === null) ||
-        value === undefined
-      ) {
-        delete paramHash[key];
-        changed = true;
-      } else {
-        if (paramHash[key] !== value) {
-          paramHash[key] = value;
-          changed = true;
-        }
-      }
-
-      // don't do work if unneeded
-      if (!changed) {
-        return [];
-      }
-
-      const keys = Object.keys(paramHash);
-      keys.sort();
-      const hash = keys
-        .map((key, i) => {
-          return `${key}=${encodeURI(paramHash[key])}`;
-        })
-        .join("&");
-      // replace after the ? but keep before that
-      let hashString = window.location.hash.substr(1);
-      const queryIndex = hashString.indexOf("?");
-      let preHashString = "";
-      if (queryIndex > -1) {
-        preHashString = hashString.substr(0, queryIndex);
-      }
-
-      const urlBlob = new URL(window.location.href);
-      urlBlob.hash = `${preHashString}?${hash}`;
-      if (opts?.modifyHistory) {
-        // adds to browser history, so affects back button
-        // fires "hashchange" event
-        window.location.hash = `${preHashString}?${hash}`;
-      } else {
-        // The following will NOT work to trigger a 'hashchange' event:
-        // Replace the state so the back button works correctly
-        window.history.replaceState(
-          null,
-          document.title,
-          `${urlBlob.pathname}${urlBlob.search}${urlBlob.hash}`
-        );
-        // Manually trigger a hashchange event:
-        // I don't know how to add the previous and new url parameters
-        window.dispatchEvent(new HashChangeEvent("hashchange"));
-      }
+      setHashParam(key, value, opts);
     },
     []
   );
 
   return [hashParam, setParam];
-};
-
-// Get everything after # then after ?
-export const getHashParams = (): [string, Record<string, string>] => {
-  let hashString = window.location.hash.substr(1);
-  const queryIndex = hashString.indexOf("?");
-  if (queryIndex === -1) {
-    return [hashString, {}];
-  }
-  const preHashString = hashString.substr(0, queryIndex);
-  hashString = hashString.substr(queryIndex + 1);
-  // @ts-ignore
-  const hashObject = Object.fromEntries(
-    hashString
-      .split("&")
-      .filter((s) => s.length > 0)
-      .map((s) => s.split("="))
-  );
-  Object.keys(hashObject).forEach(
-    (key) => (hashObject[key] = decodeURI(hashObject[key]))
-  );
-  return [preHashString, hashObject];
 };
--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,5 +1,9 @@
 import stringify from "fast-json-stable-stringify";
 
+export type SetHashParamOpts = {
+  modifyHistory?: boolean;
+};
+
 export const isIframe = (): boolean => {
   //http://stackoverflow.com/questions/326069/how-to-identify-if-a-webpage-is-being-loaded-inside-an-iframe-or-directly-into-t
   try {
@@ -21,25 +25,41 @@ export const blobFromBase64String = (value: string | undefined) => {
   return undefined;
 };
 
+export const getHashParams = (): [string, Record<string, string>] => {
+  return getUrlHashParams(window.location.href);
+};
+
+// Get everything after # then after ?
+export const getUrlHashParams = (
+  url: string
+): [string, Record<string, string>] => {
+  const urlBlob = new URL(url);
+  let hashString = urlBlob.hash.substr(1);
+  const queryIndex = hashString.indexOf("?");
+  if (queryIndex === -1) {
+    return [hashString, {}];
+  }
+  const preHashString = hashString.substr(0, queryIndex);
+  hashString = hashString.substr(queryIndex + 1);
+  // @ts-ignore
+  const hashObject = Object.fromEntries(
+    hashString
+      .split("&")
+      .filter((s) => s.length > 0)
+      .map((s) => s.split("="))
+  );
+  Object.keys(hashObject).forEach(
+    (key) => (hashObject[key] = decodeURI(hashObject[key]))
+  );
+  return [preHashString, hashObject];
+};
+
 export const getHashParamValue = (
   url: string,
   key: string
 ): string | undefined => {
-  const urlBlob = new URL(url);
-  let hashString = urlBlob.hash;
-  const queryIndex = hashString.indexOf("?");
-  if (queryIndex === -1) {
-    return;
-  }
-  hashString = hashString.substr(queryIndex + 1);
-  const params = new URLSearchParams(hashString);
-  if (params.has(key)) {
-    const value = params.get(key);
-    if (value) {
-      return value;
-    }
-  }
-  return;
+  const [_, hashParams] = getUrlHashParams(url);
+  return hashParams[key];
 };
 
 export const getHashParamObject = (
@@ -52,4 +72,65 @@ export const getHashParamObject = (
     return value;
   }
   return;
+};
+
+export const setHashParam = (
+  key: string,
+  value: string | undefined,
+  opts?: SetHashParamOpts
+) => {
+  const paramHash = getHashParams()[1];
+
+  let changed = false;
+  if (
+    (paramHash.hasOwnProperty(key) && value === null) ||
+    value === undefined
+  ) {
+    delete paramHash[key];
+    changed = true;
+  } else {
+    if (paramHash[key] !== value) {
+      paramHash[key] = value;
+      changed = true;
+    }
+  }
+
+  // don't do work if unneeded
+  if (!changed) {
+    return [];
+  }
+
+  const keys = Object.keys(paramHash);
+  keys.sort();
+  const hash = keys
+    .map((key, i) => {
+      return `${key}=${encodeURI(paramHash[key])}`;
+    })
+    .join("&");
+  // replace after the ? but keep before that
+  let hashString = window.location.hash.substr(1);
+  const queryIndex = hashString.indexOf("?");
+  let preHashString = "";
+  if (queryIndex > -1) {
+    preHashString = hashString.substr(0, queryIndex);
+  }
+
+  const urlBlob = new URL(window.location.href);
+  urlBlob.hash = `${preHashString}?${hash}`;
+  if (opts?.modifyHistory) {
+    // adds to browser history, so affects back button
+    // fires "hashchange" event
+    window.location.hash = `${preHashString}?${hash}`;
+  } else {
+    // The following will NOT work to trigger a 'hashchange' event:
+    // Replace the state so the back button works correctly
+    window.history.replaceState(
+      null,
+      document.title,
+      `${urlBlob.pathname}${urlBlob.search}${urlBlob.hash}`
+    );
+    // Manually trigger a hashchange event:
+    // I don't know how to add the previous and new url parameters
+    window.dispatchEvent(new HashChangeEvent("hashchange"));
+  }
 };
